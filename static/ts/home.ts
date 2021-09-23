45a7b84e66f9169e65f9737f42a90dc5a17b1bcb
njfamirm <njfamirm@gmail.com>
2021-09-23 14:01:03 +0330

    edit home page

--- static/ts/home.ts
+++ static/ts/home.ts
@@ -1,12 +1,31 @@
-var swapText = document.querySelector(".swap");
-function onScrollSwap() {
+function onScroll() {
+  var swapText = document.querySelector(".swap-text");
+  // var isText = document.querySelector(".is");
+  // if (window.screenY > isText.getBoundingClientRect().top) {
+  // typing();
   if (swapText === null) return;
-  if (window.scrollY > 100) {
-    (swapText as any).style.opacity = "0%";
-    (swapText as any).style.letterSpacing = "4em";
-  } else if (window.scrollY < 100) {
+  if (window.scrollY > 150) {
+    (swapText as any).style.bottom = "100vh";
+    (swapText as any).style.opacity = "0";
+  } else if (window.scrollY < 150) {
+    (swapText as any).style.bottom = "0";
     (swapText as any).style.opacity = "100%";
-    (swapText as any).style.letterSpacing = "0";
   }
 }
-document.addEventListener("scroll", onScrollSwap);
+
+document.addEventListener("scroll", onScroll);
+
+// var isText = document.querySelector(".is");
+// typing();
+// function typing() {
+//   if (isText === null) return;
+
+//   var txt = "Are these reasons not enough to use sunglasses?";
+//   var speed = 200;
+
+//   console.log(isText.innerHTML.length, txt.length);
+//   if (txt.length > isText.innerHTML.length - 1) {
+//     isText.innerHTML += txt.charAt(isText.innerHTML.length);
+//     setTimeout(typing, speed);
+//   }
+// }
